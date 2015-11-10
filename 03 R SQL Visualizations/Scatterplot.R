6f85f7c2e0561fe6824e04b970f30a5b29182468
unknown <wes.james323@gmail.com>
2015-11-10 17:10:54 -0600

    R SQL Folder, scatterplot start

--- 03 R SQL Visualizations/Scatterplot.R
+++ 03 R SQL Visualizations/Scatterplot.R
@@ -1,3 +1,8 @@
 
 df <- data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="select * from MEDICALDATA"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_gv4353', PASS='orcl_gv4353', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+df
+
+summary(df)
+head(df)
+
 
