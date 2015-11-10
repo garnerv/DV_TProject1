60529d41111d205773d7228264d2ed5930190978
unknown <wes.james323@gmail.com>
2015-11-10 17:19:58 -0600

    Barchart start

---
+++ 03 R SQL Visualizations/Barchart.R
@@ -0,0 +1,10 @@
+
+df <- data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="select * from MEDICALDATA"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_gv4353', PASS='orcl_gv4353', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+df
+
+summary(df)
+head(df)
+
+df <- MEDICALDATA %>% group_by(DRGDEFINITION, AVERAGETOTALPAYMENTS) %>% summarize(AVG_DIFF = mean(Averagetotalpayments - Averagemedicarepayments)) %>% rename(COLOR=color, CLARITY=clarity)
+# df1 <- df %>% ungroup %>% group_by(CLARITY) %>% summarize(WINDOW_AVG_PRICE=mean(AVG_PRICE))
+# df <- inner_join(df, df1, by="CLARITY")
